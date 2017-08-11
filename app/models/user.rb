a0007d59ef3ddd67dfebcb0e153dd13e389d1133
wang zhi-wen <wzwkak@gmail.com>
2017-08-12 01:48:49 +0800

    RAILS_JWT_ACCOUNT_API

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   before_save :downcase_email
   before_create :generate_confirmation_instructions
 
-  validates_presence_of :email
-  validates_uniqueness_of :email, case_sensitive: false
-  validates_format_of :email, with: /@/
+  validates_presence_of :email #空值
+  validates_uniqueness_of :email, case_sensitive: false #唯一
+  validates_format_of :email, with: /@/ #email格式
 
   def downcase_email
     self.email = self.email.delete(' ').downcase
